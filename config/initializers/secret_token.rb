928bc9318a4e98d90fbf9328902ce47b162287be
U-SYNOPSYS\tirup <tirup@tirup-e6420.internal.synopsys.com>
2013-03-10 14:48:00 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '44707b0c611a79d419d0cc6579795ba5eeb992b850f5f3eeacada97369dbfab6e455666ca01d090a65288fc7d7a648b57ecb1a3859793acc45aa72f8595089ef'
